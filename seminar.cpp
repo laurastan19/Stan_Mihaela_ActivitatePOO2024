3644dbe16d149c74c48633f2a34e2c525ab1c708
Stan Laura <laurastan1903@gmail.com>
2024-10-24 00:07:00 -0700

    sem 3

--- seminar.cpp
+++ seminar.cpp
@@ -1,67 +1,154 @@
+﻿//#include<iostream>
+//using namespace std;
+//
+//
+//struct farmacie {
+//	char* nume;
+//	string adresa;
+//	int nrmedicamente;
+//	float* preturi;
+//
+//};
+//void afisarefarmacie(farmacie f) {
+//	cout << "nume:" << f.nume << endl;
+//	cout << "adresa:" << f.adresa << endl;
+//	cout << "nr medicamente:" << f.nrmedicamente << endl;
+//	cout << "preturi:" << f.preturi[i] << ", ";
+//}
+//
+//int nrfarmacii = 3;
+//farmacie* vector = new farmacie[nrfarmacii];
+//for (int i = 0; i < nrfarmacii; i++) {
+//	vector[i] = citirefarmacie();
+//}
+//
+//for (int i = 0; i < nrfarmacii; i++) {
+//	afisarefarmacie(vector[i]);
+//}
+//
+//farmacie citirefarmacie() {
+//	farmacie f;
+//	cout << "la ce adresa e?";
+//	cin >> f.adresa;
+//	cout << "numele farmaciei:";
+//	char buffer[100];
+//	cin >> buffer;
+//	f.nume = new char[strlen(buffer) + 1];
+//	strcpy_s(f.nume, strlen(buffer) + 1, buffer);
+//	cout << "numar medicamente";
+//	cin >> f.nrmedicamente;
+//	f.preturi = new float[f.nrmedicamente];
+//	for (int i = 0; i < f.nrmedicamente; i++) {
+//		cin >> f.preturi[i];
+//	}
+//	return f;
+//}
+//
+//
+//int main() {
+//
+//	farmacie* pointer;
+//	pointer = new farmacie();
+//	pointer->adresa = "roamana";
+//
+//	pointer->nume = new char[strlen("catena") + 1];
+//	strcpy_s(pointer->nume, strlen("catena") + 1, "catena");
+//
+//	pointer->nrmedicamente = 5;
+//
+//	pointer->preturi = new float[pointer->nrmedicamente];
+//	for (int i = 0; i < pointer->nrmedicamente; i++) {
+//		pointer->preturi[i] = i + 1;
+//	}
+//	afisarefarmacie(*pointer);
+//
+//	int nrfarmacii = 3;
+//	farmacie* vector;
+//	vector = new farmacie[nrfarmacii];
+//	for (int i = 0; i < nrfarmacii; i++) {
+//		vector[i] = citirefarmacie();
+//	}
+//	for (int i = 0; i < nrfarmacii; i++) {
+//		afisarefarmacie(vector[i]);
+//	}
+
+
+//////////////////////SEMINAR 3/////////////////////////////////////////////
 #include<iostream>
+
 using namespace std;
 
+class Cofetarie {
+public:
+	string nume;
+	int nrAngajati;
+	bool esteVegana;
+	const int anDeschidere;
+	float adaos;
+	static int TVA;
 
-struct Farmacie {
-	char* nume;
-	string adresa;
-	int nrMedicamente;
-	float* preturi;
-
-};
-void afisareFarmacie(Farmacie f) {
-	cout << "Nume:" << f.nume << endl;
-	cout << "Adresa:" << f.adresa << endl;
-	cout << "Nr medicamente:" << f.nrMedicamente << endl;
-	cout << "Preturi:" << f.preturi[i] << ", ";
-}
-
-Farmacie citireFarmacie() {
-	Farmacie f;
-	cout << "La ce adresa e?";
-	cin >> f.adresa;
-	cout << "Numele farmaciei:";
-	char buffer[100];
-	cin >> buffer;
-	f.nume = new char[strlen(buffer) + 1];
-	strcpy_s(f.nume, strlen(buffer) + 1, buffer);
-	cout << "Numar medicamente";
-	cin >> f.nrMedicamente;
-	f.preturi = new float[f.nrMedicamente];
-	for (int i = 0; i < f.nrMedicamente; i++) {
-		cin >> f.preturi[i];
+	Cofetarie() : adaos(30), anDeschidere(2024) {
+		this->nume = "Delice";
+		this->nrAngajati = 3;
+		this->esteVegana = true;
 	}
-	return f;
-}
+
+	Cofetarie(string nume, int nrAngajati, bool vegan, int an, float adaos) :anDeschidere(an) {
+		this->nume = nume;
+		this->nrAngajati = nrAngajati;
+		this->esteVegana = vegan;
+		this->adaos = adaos;
+	}
+
+	void afisare() {
+		cout << "Nume:" << this->nume << endl;
+		cout << "Nr angajati:" << this->nrAngajati << endl;
+		cout << "Are produse vegane:" << (this->esteVegana ? "DA" : "NU") << endl;
+		cout << "An deschidere:" << this->anDeschidere << endl;
+		cout << "Adaos:" << this->adaos << endl;
+		cout << "TVA:" << Cofetarie::TVA << endl;
+	}
+	static void modificaTVA(int noulTVA) {
+		if (noulTVA > 0) {
+			Cofetarie::TVA = noulTVA;
+		}
+	}
+	static int calculeazaNrTotalAngajati(int nrCofetarii, Cofetarie* vector) {
+		int suma = 0;
+		for (int i = 0; i < nrCofetarii; i++) {
+			suma += vector[i].nrAngajati;
+		}
+
+		return suma;
+	}
+};
+int Cofetarie::TVA = 9;
+
 
 
 int main() {
+	Cofetarie c1;
+	c1.afisare();
+	Cofetarie::modificaTVA(19);
 
-	Farmacie* pointer;
-	pointer = new Farmacie();
-	pointer->adresa = "Roamana";
+	Cofetarie c2("DolceVita", 6, false, 2019, 10);
+	c2.afisare();
 
-	pointer->nume = new char[strlen("Catena") + 1];
-	strcpy_s(pointer->nume, strlen("Catena") + 1, "Catena");
+	Cofetarie* pointer;
+	pointer = new Cofetarie("Ana State", 3, false, 2020, 20);
+	pointer->afisare();
 
-	pointer->nrMedicamente = 5;
+	delete pointer;
+	//-> - derefentiere + accesare
+	//[] - deplasare + derefentiere
+	int nrCofetarii = 3;
+	Cofetarie* vector;
+	vector = new Cofetarie[nrCofetarii];
 
-	pointer->preturi = new float[pointer->nrMedicamente];
-	for (int i = 0; i < pointer->nrMedicamente; i++) {
-		pointer->preturi[i] = i + 1;
+	for (int i = 0; i < nrCofetarii; i++) {
+		vector[i].afisare();
 	}
-	afisareFarmacie(*pointer);
-
-	int nrFarmacii = 3;
-	Farmacie* vector;
-	vector = new Farmacie[nrFarmacii];
-	for (int i = 0; i < nrFarmacii; i++) {
-		vector[i] = citireFarmacie();
-	}
-	for (int i = 0; i < nrFarmacii; i++) {
-		afisareFarmacie(vector[i]);
-	}
-	//Farmacie** pointeri;
-	//pointeri = new Farmacie * [4];
-	return 0;
+	cout << "Numar total: " << Cofetarie::calculeazaNrTotalAngajati(nrCofetarii, vector);
+	delete[]vector;
+	return 65434574;
 }
